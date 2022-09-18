0435f3146f601e7211392d21a5c730e542246f3c
katherine <a20181823@pucp.edu.pe>
2022-09-18 16:51:43 -0500

    buscador

--- app/src/main/java/com/example/lab2/ComputadoraActivity.java
+++ app/src/main/java/com/example/lab2/ComputadoraActivity.java
@@ -1,12 +1,14 @@
 package com.example.lab2;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.PopupMenu;
 
 import com.example.lab2.entity.Computadora;
@@ -17,7 +19,7 @@ public class ComputadoraActivity extends AppCompatActivity {
 
 
     ArrayList<Computadora> computadoras = new ArrayList<>();
-
+    public String texto_buscar_comp = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +35,8 @@ public class ComputadoraActivity extends AppCompatActivity {
 
     public void btnMenuComputadoraAction(MenuItem menuItem) {
         Log.d("msg", "clic texto");
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+
         View view = findViewById(R.id.btn_menu_computadora);
         PopupMenu popupMenu = new PopupMenu(this, view);
         popupMenu.getMenuInflater().inflate(R.menu.appbar_submenu_computadora, popupMenu.getMenu());
@@ -40,6 +44,23 @@ public class ComputadoraActivity extends AppCompatActivity {
             switch (menuItem1.getItemId()) {
                 case R.id.btn_buscar_computadora:
                     Log.d("msg", "btn_buscar_computadora pressed");
+                    alertDialog.setTitle("Computadora");
+                    final EditText input = new EditText(this);
+                    alertDialog.setView(input);
+
+                    alertDialog.setPositiveButton("Buscar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            texto_buscar_comp = input.getText().toString();
+                            Log.d("msg","Texto "+ texto_buscar_comp);
+                        }
+                    });
+                    alertDialog.setNegativeButton("Cancelar", (dialogInterface, i) ->
+                            Log.d("msg","btn cancelar presionado"));
+                    alertDialog.show();
+
+
+
                     return true;
                 case R.id.btn_todo_computadora:
                     Log.d("msg", "btn_todo_computadora pressed");
